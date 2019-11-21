009da077342dd11ee1c4996866ff46a4ac1ce3e4
Katie Seggelke <katie.seggelke@gmail.com>
2019-11-21 18:04:45 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,45 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+  
+  get '/articles' do 
+    @articles = Article.all 
+    
+    erb :index 
+  end 
+  
+  get '/articles/new' do 
+    erb :new 
+  end 
+  
+  get '/articles/:id' do 
+    @article = Article.find(params[:id])
+    
+    erb :show 
+  end 
+  
+  post '/articles' do 
+    @article = Article.create(params)
+    redirect "/articles/#{@article.id}"
+    
+    erb :show 
+  end 
+  
+  get '/articles/:id/edit' do 
+    @article = Article.find(params[:id])
+    erb :edit 
+  end 
+  
+  patch '/articles/:id' do 
+    @article = Article.find(params[:id])
+
+    if @article.update(params[:article])
+      redirect "/articles/#{@article.id}"
+    else
+      puts "could not update"
+      redirect '/articles'
+    end
+  end 
+  
+  
 end
